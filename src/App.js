26175af0311338083f2f73b873e3191bddc27b3f
Diana P <dian6210@stud.kea.dk>
2021-12-01 13:32:47 +0100

    built desktop layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <img src="icons/foobar-logo.svg" alt="Foobar logo" />
+      <div className="lure">
+        <h1>
+          Locally <br /> brewed<span> beer</span>
+        </h1>
+        <img src="images/El-Hefe2.png" alt="Beer glass" />
+      </div>
+      <button>Order now</button>
     </div>
   );
 }
