7bbcef75e9541f7f29bb15b5c473f9571000836c
Diana P <dian6210@stud.kea.dk>
2021-12-03 23:08:55 +0100

    displayed products statically

---
+++ src/components/Main.js
@@ -0,0 +1,34 @@
+function Main() {
+  return (
+    <div className="Main">
+      <section className="ProductList">
+        <article>
+          <img src="images/El-Hefe.png" alt="El Hefe beer glass" />
+          <h2>El Hefe</h2>
+          <p>80 kr</p>
+          <button>About</button>
+        </article>
+        <article>
+          <img src="images/Fairy-Tale-Ale.png" alt="Fairy Tale Ale beer glass" />
+          <h2>Fairy Tale Ale</h2>
+          <p>80 kr</p>
+          <button>About</button>
+        </article>
+        <article>
+          <img src="images/GitHop.png" alt="GitHop beer glass" />
+          <h2>GitHop</h2>
+          <p>80 kr</p>
+          <button>About</button>
+        </article>
+        <article>
+          <img src="images/Hollaback-Lager.png" alt="Hollaback Lager beer glass" />
+          <h2>Hollaback Lager</h2>
+          <p>80 kr</p>
+          <button>About</button>
+        </article>
+      </section>
+    </div>
+  );
+}
+
+export default Main;
