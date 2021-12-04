7cbf8ba481af5bcfb84dcb367ca67550336f98c3
Diana P <dian6210@stud.kea.dk>
2021-12-04 17:11:02 +0100

    added plus and minus buttons to select the amount of beers to buy

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,13 +1,45 @@
+import { useState } from "react";
+
 export default function Product(props) {
-  console.log(props);
+  const [amount, setAmount] = useState(0);
+
+  // Setting path to images with the glasses
   const beerImage = `images/${props.label}`;
   const beerImageAlt = `Glass with ${props.name} label`;
+
+  // Incrementing the number of beers selected
+  function plus() {
+    setAmount((oldAmount) => {
+      if (oldAmount === 99) {
+        return 99;
+      }
+      return oldAmount + 1;
+    });
+  }
+
+  // Decrementing the number of beers selected
+  function minus() {
+    setAmount((oldAmount) => {
+      if (oldAmount > 0) {
+        return oldAmount - 1;
+      }
+      return 0;
+    });
+  }
+
   return (
     <article>
-      <img src={beerImage} alt={beerImageAlt} />
-      <h2>{props.name}</h2>
-      <p>80 kr</p>
-      <button>About</button>
+      <section>
+        <img src={beerImage} alt={beerImageAlt} />
+        <h2>{props.name}</h2>
+        <p>80 kr</p>
+        <button>About</button>
+      </section>
+      <section>
+        <button onClick={plus}>+</button>
+        <p>{amount}</p>
+        <button onClick={minus}>-</button>
+      </section>
     </article>
   );
 }
