30ff1abfe6b3c501b040cc326bfad93daaa89ef6
Diana P <dian6210@stud.kea.dk>
2021-12-15 22:44:56 +0100

    minified images

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -8,11 +8,7 @@ export default function Landing() {
         <h1>
           Locally <br /> brewed <span>beer</span>
         </h1>
-        <picture>
-          <source type="image/webp" srcSet="images/beers-webp/elhefe.webp" />
-          <source type="image/png" srcSet="images/beers-png/elhefe.png" />
-          <img src="images/beers-png/elhefe.png" alt="Glass of beer" />
-        </picture>
+        <img src="images/beers-png/elhefe.png" alt="Glass of beer" />
       </div>
       <nav>
         <Link className="order-now" to="/beers">
